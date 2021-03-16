c62ce769d07a2528817bb607901cbec0627adefa
theFirstGrade <zhenhao.chen18@student.xjtlu.edu.cn>
2021-03-17 00:02:44 +0800

    init

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,13 @@
 import * as React from "react";
+import {Button, message} from 'antd'
 
+export default class Login extends React.Component {
 
-export default class Login extends React.Component{
 
     render() {
         return (
             <div>
-                login
+
             </div>
         );
     }
